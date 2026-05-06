281067efeb7d00881749b00e80f35bbd880bcfa8
autobus <lekiaaaskya@gmail.com>
2026-05-06 03:22:56 +0300

    Стили

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,9 +1,11 @@
+import { primaryColor } from "./assets/consts";
+
 const tasks = [
-  {id: 1, name: `Task 1`, description:'Описание', completed: false},
-  {id: 2, name: `Task 2`, description:'Описание', completed: true},
-  {id: 3, name: `Task 3`, description:'Описание', completed: false},
-  {id: 4, name: `Task 4`, description:'Описание', completed: true},
-  {id: 5, name: `Task 5`, description:'Описание', completed: false},
+  {id: 1, name: `Task №1`, description:'Описание', completed: false},
+  {id: 2, name: `Task №2`, description:'Описание', completed: true},
+  {id: 3, name: `Task №3`, description:'Описание', completed: false},
+  {id: 4, name: `Task №4`, description:'Описание', completed: true},
+  {id: 5, name: `Task №5`, description:'Описание', completed: false},
 ]
 const nextId = 3;
 
@@ -11,22 +13,31 @@ function App() {
   return (
     <div className="App">
     <div className="header-container">
-        <div className="logo">logo</div>
+      <div className="logo">
+        <h1>logo</h1>
+        <img src={primaryColor} alt="purple-square-texture"/>
+      </div>
         <div className="header-container-block-1">block1</div>
+
         <div className="header-container-block-2">block2</div>
     </div>
-      <div className="tasks-container">
+    <div className="interactive-container">
+      <button className="submit-btn">Добавить таску</button>
+    </div>
+    <div className="tasks-container">
         {tasks.map(task => (
           <div key={task.id} className="task-card">
             <h1>{task.name}</h1>
             <p>{task.description}</p>
-            {/* <input value={task.completed} type="checkbox"/> */}
+            <input value={task.completed} type="checkbox"/>
           </div>
         ))}
     </div>
+
     <div className="footer-container">
 
     </div>
+
     </div>
   );  
 }
---
+++ task-list/src/assets/consts.js
@@ -0,0 +1,3 @@
+import purple_texture_with_noise from './images/primary-color-noise.png'
+
+export const primaryColor = purple_texture_with_noise
