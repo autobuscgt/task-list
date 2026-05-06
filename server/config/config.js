e64ce5338451927deac0a819935c5463b823cbd2
Масленников Владислав <vmaslennikov@mkit.local>
2026-05-06 09:55:30 +0300

    task

--- server/config/config.js
+++
@@ -1,3 +0,0 @@
-const {Sequelize} = require('sequelize')
-
-module.exports = new Sequelize({dialect:'sqlite', storage:'./config/database.sqlite'});
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const userController = require('../controllers/userController');
-const router = new Router();
-
-router.post('/login',userController.login)
-router.post('/register',userController.register)
-router.get('/getids', userController.getAll)
-
-module.exports = router;
